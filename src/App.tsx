b366c884689c5312205770dd76a8af1552567ee2
Joan Fernández Bornay <joanf81@gmail.com>
2024-07-11 20:16:24 +0200

    Fix name compinents

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Header from "./components/Sections/Header";
-import Hero from "./components/Sections/HeroSection";
+import Menu from "./components/Sections/Menu/Menu";
+import HeroSection from "./components/Sections/HeroSection";
 import LinksSection from "./components/Sections/LinksSection/LinksSection";
 import StatisticsSection from "./components/Sections/StatisticsSection/StatisticsSection";
 import CTASection from "./components/Sections/CTASection";
@@ -9,8 +9,8 @@ export default function App() {
   return (
     <>
       <div className="w-full max-w-2xl mx-auto bg-s md:max-w-3xl lg:max-w-5xl xl:max-w-7xl 2xl:max-w-screen-2xl ">
-        <Header />
-        <Hero />
+        <Menu />
+        <HeroSection />
       </div>
       <LinksSection />
       <StatisticsSection />

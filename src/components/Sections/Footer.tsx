cb7fcd5f1ffca1cfbb46eff942d8776022e1c165
Joan Fernández Bornay <joanf81@gmail.com>
2024-07-11 12:16:22 +0200

    Finish footer

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -4,23 +4,23 @@ import SocialMediaIconSvg from "../Svg/SocialMediaIconSvg";
 
 export default function Footer() {
   return (
-    <footer className="bg-veryDarkViolet">
-      <div className="flex flex-col py-16 px-6 max-w-screen-2xl mx-auto sm:flex-row sm:justify-between">
+    <footer className="bg-veryDarkViolet px-6">
+      <div className="flex flex-col py-16 max-w-screen-2xl mx-auto items-center md:flex-row md:justify-between md:items-start lg:max-w-5xl xl:max-w-7xl 2xl:max-w-screen-2xl">
         <LogoSvg />
-        <div className=" flex flex-col md:flex-row md:space-x-20">
-          <div className="flex flex-col items-start space-y-4 max-w-[120px] sm:max-w-[100px]">
+        <div className="flex flex-col mt-16 md:flex-row md:space-x-20 md:mt-0">
+          <div className="flex flex-col items-center text-center space-y-4 mb-16 md:max-w-[100px] md:items-start md:text-left">
             <p className="font-bold text-white">Features</p>
             <FooterLink>Link Shortening</FooterLink>
             <FooterLink>Branded Links</FooterLink>
             <FooterLink>Analytics</FooterLink>
           </div>
-          <div className="flex flex-col items-start space-y-4 max-w-[120px] sm:max-w-[100px]">
+          <div className="flex flex-col items-center text-center space-y-4 mb-16 md:max-w-[100px] md:items-start md:text-left">
             <p className="font-bold text-white">Resources</p>
             <FooterLink>Blog</FooterLink>
             <FooterLink>Developers</FooterLink>
             <FooterLink>Suport</FooterLink>
           </div>
-          <div className="flex flex-col items-start space-y-4 max-w-[120px] sm:max-w-[100px]">
+          <div className="flex flex-col items-center text-center space-y-4 mb-16 md:max-w-[100px] md:items-start md:text-left">
             <p className="font-bold text-white">Company</p>
             <FooterLink>About</FooterLink>
             <FooterLink>Our team</FooterLink>
@@ -36,7 +36,10 @@ export default function Footer() {
             />
           </FooterLink>
           <FooterLink>
-            <SocialMediaIconSvg iconType="X" className="hover:fill-cyan hover:scale-110 duration-100" />
+            <SocialMediaIconSvg
+              iconType="X"
+              className="hover:fill-cyan hover:scale-110 duration-100"
+            />
           </FooterLink>
           <FooterLink>
             <SocialMediaIconSvg

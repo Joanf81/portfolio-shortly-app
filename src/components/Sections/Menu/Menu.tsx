af2465a374c6582dd966a90b72f1b70a0ca77e83
Joan Fernández Bornay <joanf81@gmail.com>
2024-07-11 20:15:41 +0200

    Menu moved

---
+++ src/components/Sections/Menu/Menu.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+import Link from "../../HtmlElements/Link";
+import Button from "../../HtmlElements/Button";
+import LogoSvg from "../../Svg/LogoSvg";
+import HamburgerButton from "./HamburguerButton";
+import HamburgerMenu from "./HamburgerMenu";
+
+export default function Menu() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  function handleHamburguerButtonClick() {
+    if (open) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  }
+
+  return (
+    <menu className="relative">
+      <div className="flex flex-row items-center justify-between w-full pt-6 px-6">
+        <div className="flex flex-row items-center space-x-20">
+          <LogoSvg />
+          <div className="hidden flex-row items-center space-x-8 lg:flex">
+            <Link href="#">Features</Link>
+            <Link href="#">Pricing</Link>
+            <Link href="#">Resources</Link>
+          </div>
+        </div>
+        <div className="hidden flex-row items-center space-x-6 lg:flex">
+          <Link href="#">Login</Link>
+          <Button>Sign Up</Button>
+        </div>
+        <HamburgerButton
+          className="lg:hidden"
+          open={open}
+          handleClick={handleHamburguerButtonClick}
+        />
+      </div>
+      <HamburgerMenu open={open} />
+    </menu>
+  );
+}
